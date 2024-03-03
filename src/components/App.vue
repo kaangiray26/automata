9d84a80267788aa4d1ea07612f0cefcf89ba9c8a
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-03-03 16:04:22 +0100

    adding zoom

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,31 @@
 <template>
     <div ref="container" class="container manrope-bold py-4">
         <h1 class="mb-3">Cellular Automata Compression</h1>
-        <canvas @mousedown="mousedown" @mouseup="mouseup" @mousemove="mousemove" @mouseleave="mouseup"
-            @click="click"></canvas>
-        <div class="d-flex mt-3">
-            <button @click="switch_mode">{{ editing ? "Editing" : "Viewing" }}</button>
-            <button @click="toggle_simulation">{{ running ? "Stop" : "Start" }}</button>
+        <div class="d-flex align-items-center">
+            <button @click="switch_mode">{{ editing ? "Edit mode" : "View mode" }}</button>
         </div>
+        <div class="d-flex position-relative mt-3">
+            <canvas @mousedown="mousedown" @mouseup="mouseup" @mousemove="mousemove" @mouseleave="mouseup"
+                @click="click">
+            </canvas>
+            <div class="controls">
+                <span>Step: {{ step }}</span>
+                <div class="d-flex">
+                    <span class="me-2">Speed: {{ delay }}</span>
+                    <input type="range" min="0" max="1000" v-model="delay" @input="change_interval">
+                </div>
+                <div class="d-flex">
+                    <span class="me-2">Zoom: {{ scale }}</span>
+                    <input type="range" min="0" max="10" @input="change_zoom" value="1">
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-column mt-3">
+            <div class="d-flex align-items-center">
+                <button @click="toggle_simulation">{{ running ? "Stop" : "Start" }}</button>
+            </div>
+        </div>
+        <div class="d-flex flex-column"></div>
     </div>
 </template>
 
@@ -18,10 +37,13 @@ const size = ref({
     width: 0,
     height: 0,
 })
+
 const cells = ref({});
-const editing = ref(false);
+const editing = ref(true);
 const running = ref(false);
 
+const step = ref(0);
+const delay = ref(250);
 const interval = ref(null);
 const container = ref(null);
 
@@ -35,18 +57,24 @@ let canvas = null;
 let ctx = null;
 
 let scale = 1;
-
-let rx = 0,
-    ry = 0;
-let left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0;
+let scaleStep = 0.1;
+let lastScale = scale;
+let maxScale = 10;
+let minScale = 0.1;
 
 let zoomPoint = {
     x: 0,
     y: 0
 };
+let lastZoomPoint = zoomPoint;
+
+let rx = 0,
+    ry = 0;
+
+let left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0;
 
 const rules = {
     "0000": 0,
@@ -76,6 +104,21 @@ function resizeCanvas() {
     // Set canvas size
     canvas.width = size.value.width * size.value.dpi;
     canvas.height = size.value.height * size.value.dpi;
+}
+
+function calculate() {
+    calculateDistancesToCellBorders();
+    calculateDrawingPositions();
+}
+
+function calculateDistancesToCellBorders() {
+    let dx = zoomPoint.x - lastZoomPoint.x + rx * lastScale;
+    rx = dx - Math.floor(dx / (lastScale * cellSize)) * lastScale * cellSize;
+    rx /= lastScale;
+
+    let dy = zoomPoint.y - lastZoomPoint.y + ry * lastScale;
+    ry = dy - Math.floor(dy / (lastScale * cellSize)) * lastScale * cellSize;
+    ry /= lastScale;
 }
 
 function calculateDrawingPositions() {
@@ -188,6 +231,33 @@ function mousemove(event) {
     update();
 }
 
+function wheel(e) {
+    zoom(e.deltaY > 0 ? -1 : 1, {
+        x: e.offsetX,
+        y: e.offsetY
+    });
+
+    calculate();
+    update();
+}
+
+function zoom(amt, point) {
+    lastScale = scale;
+    scale += amt * scaleStep;
+
+    if (scale < minScale) {
+        scale = minScale;
+    }
+
+    if (scale > maxScale) {
+        scale = maxScale;
+    }
+
+    lastZoomPoint = zoomPoint;
+    zoomPoint = point;
+}
+
+
 async function click(event) {
     // Check if editing
     if (!editing.value) return;
@@ -212,21 +282,32 @@ async function switch_mode() {
 }
 
 async function toggle_simulation() {
+    editing.value = false;
     running.value = !running.value;
 
     if (running.value) {
         // Start simulation
-        interval.value = setInterval(simulation_step, 1000);
+        interval.value = setInterval(simulation_step, 1000 - delay.value);
     } else {
         // Stop simulation
         clearInterval(interval.value);
     }
 }
 
-function simulation_step() {
-    // Calculate next step
-    console.log("Cells:", cells.value);
+async function change_interval() {
+    if (!running.value) return;
 
+    clearInterval(interval.value);
+    interval.value = setInterval(simulation_step, 1000 - delay.value);
+}
+
+async function change_zoom() {
+    //
+}
+
+function simulation_step() {
+    console.log("Step:", step.value);
+    // Calculate next step
     let blocks = {};
 
     // For each cell, find its corresponding 2x2 block
@@ -247,10 +328,120 @@ function simulation_step() {
     }
 
     // Calculate next step
-    console.log("Blocks:", blocks);
+    for (let key of Object.keys(blocks)) {
+        let configuration = blocks[key].join("");
+        let rule = rules[configuration];
+        enforce_rules(key, rule);
+    }
+
+    // Redraw
+    update();
+
+    // Increase step
+    step.value++;
+}
+
+function enforce_rules(block, rule) {
+    // Coordinates for the upper left cell on block
+    let [x, y] = block.split(",").map(Number);
+    x *= 2;
+    y *= 2;
+
+    // 16 rules
+    switch (rule) {
+        case 0:
+            x -= 1;
+            y -= 1;
+            break;
+        case 1:
+            y -= 1;
+            break;
+        case 2:
+            x += 1;
+            y -= 1;
+            break;
+        case 3:
+            x += 2;
+            y -= 1;
+            break;
+        case 4:
+            x += 2;
+            break;
+        case 5:
+            x += 2;
+            y += 1;
+            break;
+        case 6:
+            x += 2;
+            y += 2;
+            break;
+        case 7:
+            x += 1;
+            y += 2;
+            break;
+        case 8:
+            y += 2;
+            break;
+        case 9:
+            x -= 1;
+            y += 2;
+            break;
+        case 10:
+            x -= 1;
+            y += 1;
+            break;
+        case 11:
+            x -= 1;
+            break;
+        case 12:
+            break;
+        case 13:
+            x += 1;
+            break;
+        case 14:
+            x += 1;
+            y += 1;
+            break;
+        case 15:
+            y += 1;
+            break;
+    }
+
+    // cell to change
+    let cell = `${x},${y}`;
+
+    // Change value of cell
+    if (cells.value[cell]) delete cells.value[cell];
+    else cells.value[cell] = 1;
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
 
 onMounted(() => {
+    // Randomize rules values
+    let values = Array.from({ length: 16 }, (_, i) => i);
+    values = shuffle(values);
+    for (let i = 0; i < 16; i++) {
+        rules[Object.keys(rules)[i]] = values[i];
+    }
+    console.log("Rules:", rules);
+
     // Get canvas and context
     canvas = document.querySelector("canvas");
     ctx = canvas.getContext("2d");
@@ -260,7 +451,7 @@ onMounted(() => {
 
     // Move slightly
     move(32, 32);
-    calculateDrawingPositions();
+    calculate();
 
     // Draw grid
     draw();
